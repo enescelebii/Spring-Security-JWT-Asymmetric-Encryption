224ea575300df1a86ab5408b599b817a06080856
Enes <ereseres1579@gmail.com>
2026-01-13 02:52:49 +0300

    fix: update OpenApiConfig for improved documentation and server configuration

--- src/main/java/com/vena/app/config/OpenApiConfig.java
+++ src/main/java/com/vena/app/config/OpenApiConfig.java
@@ -1,42 +1,35 @@
 package com.vena.app.config;
 
-
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @OpenAPIDefinition(
         info = @Info(
                 contact = @Contact(
-                        name = "Spring security JWT asymmetric Encryption demo",
+                        name = "Enes Celebi",
                         email = "enescelebi222@gmail.com",
                         url = "https://github.com/enescelebii"
                 ),
                 description = "OpenApi documentation for Spring security project",
                 title = "OpenAPI Specification",
-                version = "1.0",
-                license = @License(
-                        name = "Licence name",
-                        url = "https://github.com/enescelebii"
-                ),
-                termsOfService = "Terms of service"
+                version = "1.0"
         ),
         servers = {
                 @Server(
-                        url = "http://localhost:8080",
-                        description = "Local ENV"
+                        url = "http://129.159.220.126:8080",
+                        description = "Dev Server (Oracle VPS)"
                 ),
                 @Server(
-                        // example server
-                        url = "https://prod.url",
-                        description = "Prod ENV"
-
+                        url = "http://localhost:8080",
+                        description = "Local ENV"
                 )
         },
         security = {
